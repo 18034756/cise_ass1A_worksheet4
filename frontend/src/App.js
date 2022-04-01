a2c1a1ee2b71a0a11beb08faf5da1d529af01ae7
Jinpeng Zhang <tyn5682@autuni.ac.nz>
2022-04-01 18:27:10 +1300

    first commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
+import {
+  Route,
+  NavLink,
+  Routes,
+  BrowserRouter as Router
+} from "react-router-dom";
 
-import{
-  BrowserRouter as Router,
-}from "react-router-dom";
+import Home from "./pages/Home";
+import SEPractice from "./pages/SE-Practice";
+import SubmitArticle from "./pages/Submit-Article"; 
+import NotFoundPage from "./pages/404";
 
 const App = () =>  {
     return (
@@ -10,12 +17,19 @@ const App = () =>  {
         <div>
           <h1>Software Engineering Practice Evidence Repository (SEPER)</h1>
             <ul className="header">
-                <li><a href = "/">Home</a></li>
-                <li><a href = "/SEPractice">Select the Practice</a></li>
-                <li><a href = "/SubmitArticle">Submit an Article</a></li>
+                <li><NavLink exact to = "/">Home</NavLink></li>
+                <li><NavLink to = "/SEPractice">Select the Practice</NavLink></li>
+                <li><NavLink to = "/SubmitArticle">Submit an Article</NavLink></li>
+                <li><NavLink to = "/404">Not Found Page</NavLink></li>
+
             </ul>
           <div className="content">
-            
+            <Routes>
+            <Route exact path="/" element={<Home />}/>
+            <Route  path="/SEPractice" element={<SEPractice />}/>
+            <Route  path="/SubmitArticle" element={<SubmitArticle />}/>
+            <Route exact path="/404"  element={<NotFoundPage />}/>
+            </Routes>
           </div>
         </div>
         </Router>
